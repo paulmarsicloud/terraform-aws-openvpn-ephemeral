6e5638af16238d8abcf5214d1637988cf2150b3d
Paul Marsicovetere <paulmarsicloud@gmail.com>
2021-08-05 15:15:40 -0400

    variables-update

--- variables.tf
+++ variables.tf
@@ -14,11 +14,9 @@ variable "output_suppression" {
 variable "public_ip" {
   type        = string
   description = "user's personal public ip to access ec2 instance"
-  default     = ""
 }
 
 variable "region" {
   type        = string
   description = "region to deploy openvpn ec2 resource"
-  default     = ""
 }
