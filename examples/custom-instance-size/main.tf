a4f2b32a9c5e310f209f26a35e31991f6cf6cac8
Paul Marsicovetere <paul.marsicovetere@formidable.com>
2022-09-30 09:24:38 -0400

    patch update, documentation

---
+++ examples/custom-instance-size/main.tf
@@ -0,0 +1,15 @@
+variable "region" {}
+variable "public_ip" {}
+variable "instance_type" {}
+
+terraform {
+  backend "s3" {}
+}
+
+module "openvpn-ephemeral" {
+  source        = "paulmarsicloud/openvpn-ephemeral/aws"
+  version       = "1.0.1"
+  region        = var.region
+  public_ip     = var.public_ip
+  instance_type = var.instance_type
+}
